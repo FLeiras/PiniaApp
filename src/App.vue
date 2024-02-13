1e0b54fdc0e45a195ef45fac5470ccb3ac8e9d0d
FedericoL_exo <federicol@exo.com.ar>
2024-02-13 18:40:47 -0300

    feat(server):Integracion y creacion del server con json-server

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@ import IconPinia from '@/shared/components/icons/IconPinia.vue';
       <nav>
         <RouterLink to="/">Counter Options</RouterLink>
         <RouterLink to="/counter-2">Counter Setup</RouterLink>
+        <RouterLink to="/clients">Clientes</RouterLink>
       </nav>
     </div>
   </header>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,10 @@
-import CounterValue from '@/counter/pages/Counter-1-page.vue'
-import CounterSetupPage from '@/counter/pages/CounterSetupPage.vue'
 import { createRouter, createWebHistory } from 'vue-router'
+
+import CounterValue from '@/counter/pages/Counter-1-page.vue'
+import ClientsLayout from '@/clients/layout/ClientsLayout.vue'
+import CounterSetupPage from '@/counter/pages/CounterSetupPage.vue'
+import ListPage from '@/clients/pages/ListPage.vue'
+import ClientPage from '@/clients/pages/ClientPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +18,16 @@ const router = createRouter({
       path: '/counter-2',
       name: 'counter-2',
       component: CounterSetupPage
+    },
+    {
+      path: '/clients',
+      name: 'clients',
+      component: ClientsLayout,
+      redirect: {name: 'list'},
+      children: [
+        { path: '/clients', name: 'list', component: ListPage },
+        { path: '/clients/:id', name: 'client', component: ClientPage },
+      ]
     },
   ]
 })
